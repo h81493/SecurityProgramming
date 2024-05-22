e6afd0f1f2518d14bf6f23a3c308b05dcd18870a
h81493 <h81493@g.neec.ac.jp>
2024-05-22 10:38:51 +0900

    変数名の見直し.

--- S04/test04.c
+++ S04/test04.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-  int a=1;
+  int a1=1;
   {
-    int a=2;
-    printf("block2 a=%d\n",a);
+    int a2=2;
+    printf("block2 a=%d\n",a2);
   }
-  printf("block1 a=%d\n",a);
+  printf("block1 a=%d\n",a1);
+  return(0);
 }
