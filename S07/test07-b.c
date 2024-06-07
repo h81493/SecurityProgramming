ee104b052245f7dd38250b42fcc0513a0c00214a
h81493 <h81493@g.neec.ac.jp>
2024-06-07 14:50:46 +0900

    fix test07-b.c

--- S07/test07-b.c
+++ S07/test07-b.c
@@ -4,7 +4,7 @@ int main( void) {
   for( int wday = 0; wday < 7; wday++) {
     for( int i = 0; i < 3; i++)
       printf("%c","SunMunTueWedThuFriSat"[wday * 3 + i]);
-      printf(" ");
+    printf(" ");
   }
   return 0;
 }
