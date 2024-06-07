8d6dc5ff349c4a90c24af1dc059b85db19c589bf
h81493 <h81493@g.neec.ac.jp>
2024-06-07 14:52:48 +0900

    fix test07-c.c

--- S07/test07-c.c
+++ S07/test07-c.c
@@ -4,7 +4,7 @@ int main( void) {
   for( int wday = 0; wday < 7; wday++) {
     for( int i = 0; i < 3; i++)
       printf("%c","日月火水木金土"[wday * 3 + i]);
-      printf(" ");
+    printf(" ");
   }
   return 0;
 }
